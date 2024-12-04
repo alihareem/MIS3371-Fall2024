7baa5201d3fcf1fae4471c27430bb36f507fde54
alihareem <hareemali2003@gmail.com>
2024-12-03 19:15:13 -0600

    Update Homework4.js

--- Homework4.js
+++ Homework4.js
@@ -482,16 +482,16 @@ function setCookie (name, cvalue, expiryDays) {
   document.cookie = name + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-function getCookie {
+function getCookie (name) {
  var cookieNmae = name + "=";
  var cookies = document.cookie.split(';');
 
  for (var i=0; i < cookies.length; i++) {
   var cookie = cookies[i].trim();
-  while (cookue.charAt (0) == ' '{
+  while (cookie.charAt (0) == ' ') {
    cookie = cookie.substring(1);
   }
-  if (cookie.indexOf(cookieName) == 0{
+  if (cookie.indexOf(cookieName) == 0) {
    return cookie.substring(cookieName.length, cookie.length);
   }
  }
